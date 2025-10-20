108bc82966bd24d04b22630a283e82c409c3c790
mahmoodana1 <arabg1999@gmail.com>
2025-10-20 19:23:29 +0300

    Added the . option to the addFile function

--- commands/add.cpp
+++ commands/add.cpp
@@ -1,14 +1,39 @@
 #include <filesystem>
 #include <iostream>
+#include <string>
 
 void fileAdd(const std::string &fileName) {
     namespace fs = std::filesystem;
+
+    if (fileName == ".") {
+        fs::remove_all("./.minigit/addedFiles");
+        fs::create_directory("./.minigit/addedFiles");
+
+        std::string path = ".";
+
+        for (const auto &entry : fs::recursive_directory_iterator(path)) {
+            if (entry.path().string().find("/.git") != std::string::npos ||
+                entry.path().string().find("/.minigit") != std::string::npos) {
+                continue;
+            }
+
+            if (fs::is_directory(entry.path())) {
+                fs::create_directories((fs::path)("./.minigit/addedFiles") /
+                                       entry.path());
+                continue;
+            }
+
+            fs::copy_file(entry.path(),
+                          ((fs::path) "./.minigit/addedFiles") / entry.path());
+        }
+
+        return;
+    }
 
     if (!fs::exists(fileName)) {
         std::cout << "File doesn't exist";
         return;
     }
-
     if (fs::exists(".minigit/addedFiles/" + fileName)) {
         fs::remove(".minigit/addedFiles/" + fileName);
     }
